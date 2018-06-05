2168d38cfce637498402d5e096eeb53ec83a809e
Wilson Mar <wilsonmar+github@gmail.com>
2018-06-05 11:01:12 -0600

    Update hygieia.sh

--- aws/hygieia.sh
+++ aws/hygieia.sh
@@ -1,2 +1,7 @@
+#!/bin/bash -e
+
+# Create within EC2 a Hybieia instance, then edit the configuration.
+# sh -c "$(curl -fsSL https://github.com/github-kbaker/DevSecOps/blob/master/aws/hygieia.sh)"
+
 git clone https://github.com/capitalone/Hygieia.git
 
