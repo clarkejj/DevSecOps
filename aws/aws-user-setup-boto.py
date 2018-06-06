d0367741312f5ddcef15c13609be9b83fb5dcacc
Wilson Mar <wilsonmar+github@gmail.com>
2018-06-06 15:38:12 -0600

    Create aws-user-setup-boto.py

---
+++ aws/aws-user-setup-boto.py
@@ -0,0 +1,23 @@
+
+# From https://gist.github.com/sebsto/9a958ff1c761b8c7c90d
+
+import json, boto
+
+# Connect to IAM with boto
+iam = boto.connect_iam(ACCESS_KEY, SECRET_KEY)
+
+# Create user
+user_response = iam.create_user('aws-user')
+
+# Create Policy
+policy = { 'Version' : '2012-10-17'}
+policy['Statement'] = [{'Sid' : 'AwsIamUserPython', 
+                        'Effect': 'Allow', 
+                        'Action': 's3:*', 
+                        'Resource': 'arn:aws:s3:::class-rocks/*'}]
+policy_json = json.dumps(policy, indent=2)
+
+iam.put_user_policy('aws-user', 'allow_access_class-rocks', policy_json)
+
+# Generate new access key pair for 'aws-user'
+key_response = iam.create_access_key('aws-user')
