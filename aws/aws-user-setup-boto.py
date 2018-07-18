cd510dd0d4f87fb42e79e8139aa988855ddf800e
Wilson Mar <wilsonmar@gmail.com>
2018-07-18 10:06:32 -0600

    Update aws-user-setup-boto.py

--- aws/aws-user-setup-boto.py
+++ aws/aws-user-setup-boto.py
@@ -1,5 +1,6 @@
 
-# From https://gist.github.com/sebsto/9a958ff1c761b8c7c90d
+# This assumes enviornment variables for AWS ACCESS_KEY and SECRET_KEY have been established.
+# Based on https://gist.github.com/sebsto/9a958ff1c761b8c7c90d
 
 import json, boto
 
