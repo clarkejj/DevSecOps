77f3caf38b13481f3c3fa575c2ecfe8411ddb61d
Wilson Mar <wilsonmar@gmail.com>
2018-08-03 11:35:57 -0600

    Update linux-install.sh

--- AppD/linux-install.sh
+++ AppD/linux-install.sh
@@ -6,9 +6,14 @@ VM_IP="129.146.159.96"
 
 # In a Terminal, after you > YES | ssh ravello@129.146.159.96 and supply password, paste this:
 # At [ravello@Java101Template ~]$ 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/appd-setup.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/AppD/linux-install.sh)"
+# https://github.com/wilsonmar/DevSecOps/blob/master/AppD/linux-install.sh
 
-echo $PWD  # /home/ravello
+cat /etc/issue  # says Debian release 6.7 (Final)
+
+echo $PWD  # says /home/ravello
+ls -al  # to see starter files in image
+
 sudo su - tomcat
 # echo $PWD  # /home/tomcat
 
@@ -19,10 +24,30 @@ echo $TOMCAT_HOME
 chmod +x $TOMCAT_HOME/bin/*.sh
 $TOMCAT_HOME/bin/catalina.sh start  # for Tomcat started.
 # Get TOMCAT_PID
-TOMCAT_PID="6145"
-#TOMCAT_PID=$(ps -ef | grep "opt/tomcat" | ???)
-netstat -natp | grep $TOMCAT_PID
+PID="$(ps x | grep -m1 '/tomcat' | awk '{print $1}')" ; echo $PID
+netstat -natp | grep $PID
 
 # On your local machine: open "http://129.146.159.96/Cars_Sample_App/home.do"
 # to see "Supercar trader" page
 
+#### Switch back to admin user:
+# Load USER_PASSWORD
+# echo $USER_PASSWORD | sudo -S su ravello
+pause
+cd /home/ravello
+
+#### Download Java agent:
+FILE_DOWNLOAD="AppServerAgent-4.5.0.23604.zip"
+# From https://elephant201808020420052.saas.appdynamics.com:443 saved to my personal Google Drive:
+wget -O "$FILE_DOWNLOAD" https://drive.google.com/open?id=1Kz1XlN_0tk3vrptQw1msfCDBeiNVb48L
+# TODO: Verify SSH hash
+ls -al "$FILE_DOWNLOAD"  # should say 127833
+# tar -xvf "$FILE_DOWNLOAD"  # not jar xvf or unzip 
+
+#### Download DB agent:
+FILE_DOWNLOAD="dbagent-4.5.0.671.zip"
+# From https://elephant201808020420052.saas.appdynamics.com:443 saved to my personal Google Drive:
+wget -O "$FILE_DOWNLOAD" https://drive.google.com/open?id=12Jf_kU8Xmj7k9GPvvTdLdnYumqO5aw-4
+# TODO: Verify SSH hash
+ls -al "$FILE_DOWNLOAD"  # should say 127787
+jar xvf "$FILE_DOWNLOAD" # not # tar -xvf "$FILE_DOWNLOAD"  # not or unzip 
