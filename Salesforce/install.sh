92c61312abb93dc67d4f86255a87e5086f8c0720
Wilson Mar <wilsonmar@gmail.com>
2018-08-09 09:20:23 -0600

    Create install.sh

---
+++ Salesforce/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Based on https://github.com/dreamhouseapp/dreamhouse-sfdx/blob/master/scripts/orgInit.sh
+
+DURATION=7
+
+if [ "$#" -eq 1 ]; then
+  DURATION=$1
+fi
+
+sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -f $DURATION
+sfdx force:source:push
+sfdx force:user:permset:assign -n dreamhouse
+sfdx force:org:open -p /lightning/page/home
+echo "Org is set up"
