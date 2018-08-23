75e09e9af6dcf4a79038ada6f2bbc4e42a8f2fa9
Wilson Mar <WilsonMar+GitHub@gmail.com>
2018-08-23 15:05:25 -0600

    try regex answers

--- Kakunin/kakunin-init-expect.sh
+++ Kakunin/kakunin-init-expect.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/expect -f 
 # File kakunin-init-expect.sh
+   set timeout -1  # to avoid timeout
 spawn npm run kakunin init
 #   expect "? What kind of application would you like to test?"
    expect "Answer: "
    send -- "3\r"
 #   expect "? What is base url? [http://localhost:3000]"
-   expect "Answer: "
-#   expect "{[#>$]}"         #expect several prompts, like #,$ and >
+   expect "{[#>$]}"         #expect several prompts, like #,$ and >
    send -- "http://todomvc.com\r"
 #   expect "? What kind of email service would you like to use?"
    expect "Answer: "
