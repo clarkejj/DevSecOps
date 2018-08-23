4d0d80c330c8b029cfd901fcc21ac9bc7cd8f7f8
Wilson Mar <WilsonMar+GitHub@gmail.com>
2018-08-23 17:01:25 -0600

    take out question marks

--- Kakunin/kakunin-init-expect.sh
+++ Kakunin/kakunin-init-expect.sh
@@ -5,10 +5,11 @@
 #   set timeout -1  # wait to avoid timeout
 spawn npm run kakunin init
 #   expect "? What kind of application would you like to test?"
+#  "/r" in for return key.
    expect "Answer: "
    send -- "3\r"
 
-   expect "\? What is base url\? [http://localhost:3000]"
+   expect "? What is base url? [http://localhost:3000]"
 #   expect "{[#>$]}"         #expect several prompts, like #,$ and >
    send -- "http://todomvc.com\r"
 
