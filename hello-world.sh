df781b722725523e71f8e00ba43b3dd56bf17144
wilsonmar <wilsonmar@gmail.com>
2018-09-14 06:28:31 -0600

    hello

---
+++ hello-world.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This is hello-world.sh from https://github.com/wilsonmar/DevSecOps/
+# by WilsonMar@gmail.com
+# Described in https://
+
+# This script is used to verify that scripts can run
+# Copy this command and paste in your terminal:
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/hello-world.sh)"
+
+echo "Hello world!"
