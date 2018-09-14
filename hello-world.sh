46373d6862335b24406a9eea4887c8af0e7ef543
wilsonmar <wilsonmar@gmail.com>
2018-09-14 07:06:50 -0600

    hello I'm Windows

--- hello-world.sh
+++ hello-world.sh
@@ -12,15 +12,24 @@ echo "Hello world!"
 ### OS detection:
 platform='unknown'
 unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
+if [[ "$unamestr" == 'Darwin' ]]; then
+   platform='macos'
+elif [[ "$unamestr" == 'Linux' ]]; then
    platform='linux'
 elif [[ "$unamestr" == 'FreeBSD' ]]; then
    platform='freebsd'
+elif [[ "$unamestr" == 'Windows' ]]; then
+   platform='windows'
 fi
 echo "I'm $unamestr = $platform"
 
-if [[ $platform == 'linux' ]]; then
+
+  if [[ $platform == 'macos' ]]; then
+   alias ls='ls --color=auto'
+elif [[ $platform == 'linux' ]]; then
    alias ls='ls --color=auto'
 elif [[ $platform == 'freebsd' ]]; then
    alias ls='ls -G'
+elif [[ $platform == 'windows' ]]; then
+   alias ls='dir'
 fi
