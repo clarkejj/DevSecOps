3d319208ff7e152fd315adce0b867e2efa8c492c
wilsonmar <wilsonmar@gmail.com>
2018-09-14 07:01:48 -0600

    hello with os detection

--- hello-world.sh
+++ hello-world.sh
@@ -8,3 +8,18 @@
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/hello-world.sh)"
 
 echo "Hello world!"
+
+platform='unknown'
+unamestr=`uname`
+echo "$unamestr"
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+fi
+
+if [[ $platform == 'linux' ]]; then
+   alias ls='ls --color=auto'
+elif [[ $platform == 'freebsd' ]]; then
+   alias ls='ls -G'
+fi
