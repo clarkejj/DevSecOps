2f2f4c708f97cceca57b4203f269c0805b7c5630
Wilson Mar <wilsonmar@gmail.com>
2018-11-13 15:01:33 -0700

    Update jmeter-mac-demo.sh

--- JMeter/jmeter-mac-demo.sh
+++ JMeter/jmeter-mac-demo.sh
@@ -1,3 +1,10 @@
 #!/bash
+# From https://github.com/wilsonmar/DevSecOps/blob/master/JMeter/jmeter-mac-demo.sh
+# by WilsonMar@gmail.com
 
-# This is an example
+# This installs and configures JMeter on a Mac using Homebrew.
+
+# Copy the command below (without the #) 
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/JMeter/jmeter-mac-demo.sh)"
+
+echo "Hello $0"
