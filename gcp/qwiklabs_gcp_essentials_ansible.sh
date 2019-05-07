15872864fb65af359468c4b2722b34ece17f3258
John Clarke <clarkejjm@gmail.com>
2019-05-07 07:22:26 -0600

    init comments and links

---
+++ gcp/qwiklabs_gcp_essentials_ansible.sh
@@ -0,0 +1,8 @@
+#
+# Learn how to automate Google Cloud with Ansible and Qwiklabs 
+# https://blog.jasoncallaway.com/2018/02/04/learn-how-to-automate-google-cloud-with-ansible-and-qwiklabs/
+# Where is this lab!? Ansible automation for the Qwiklabs GCE Essentials Quest
+# https://github.com/jason-callaway/qwiklabs-gcp-essentials-ansible
+#
+# Gary Stafford - Getting Started with Red Hat Ansible for Google Cloud Platform
+# https://programmaticponderings.com/2019/01/30/getting-started-with-red-hat-ansible-for-google-cloud-platform/
